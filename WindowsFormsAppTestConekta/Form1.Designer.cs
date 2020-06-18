c52f975a520394d32415397454c18e15c93e5d83
omarus85 <52642246+omarus85@users.noreply.github.com>
2020-06-17 20:55:47 -0500

    Linea I
    linea i

--- WindowsFormsAppTestConekta/Form1.Designer.cs
+++ WindowsFormsAppTestConekta/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonAccept = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(12, 12);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(758, 134);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 224);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(758, 317);
+            this.textBoxOutput.TabIndex = 1;
+            // 
+            // buttonAccept
+            // 
+            this.buttonAccept.Location = new System.Drawing.Point(12, 174);
+            this.buttonAccept.Name = "buttonAccept";
+            this.buttonAccept.Size = new System.Drawing.Size(75, 23);
+            this.buttonAccept.TabIndex = 2;
+            this.buttonAccept.Text = "Aceptar";
+            this.buttonAccept.UseVisualStyleBackColor = true;
+            this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.buttonAccept);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonAccept;
     }
 }
 
--- WindowsFormsAppTestConekta/Form1.cs
+++ WindowsFormsAppTestConekta/Form1.cs
@@ -12,9 +12,63 @@ namespace WindowsFormsAppTestConekta
 {
     public partial class Form1 : Form
     {
+        private int M = 0, N = 0;
+        private int[,] image;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void buttonAccept_Click(object sender, EventArgs e)
+        {
+            foreach (string linea in textBoxInput.Lines)
+            {
+                switch (linea[0])
+                {
+                    case 'I':
+                        Iline(linea);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        private void Iline(string line)
+        {
+            string[] list = line.Split(' ');
+            try
+            {
+                if (list.Length == 3)
+                {
+                    if (list[0] != "I")
+                        throw new Exception();
+                    if (!int.TryParse(list[1], out M))
+                        throw new Exception();
+                    if (!int.TryParse(list[2], out N))
+                        throw new Exception();
+                    createImage(M, N);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Linea no válida: " + line);
+            }
+
+        }
+
+        private void createImage(int M, int N)
+        {
+            MessageBox.Show("creaimagen: " + M + " " + N);
+            image = new int[M, N];
+        }
+
     }
 }
