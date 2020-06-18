40f466ee8cb8e082e7e4e5fa8d3a77a349cd47bc
omarus85 <52642246+omarus85@users.noreply.github.com>
2020-06-17 21:31:24 -0500

    C and L line

--- WindowsFormsAppTestConekta/Form1.cs
+++ WindowsFormsAppTestConekta/Form1.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsAppTestConekta
     public partial class Form1 : Form
     {
         private int M = 0, N = 0;
-        private int[,] image;
+        private char[,] image;
 
         public Form1()
         {
@@ -27,15 +27,33 @@ namespace WindowsFormsAppTestConekta
                 switch (linea[0])
                 {
                     case 'I':
-                        Iline(linea);
+                        Iline(linea.Trim());
+                        break;
+                    case 'C':
+                        Cline(linea.Trim());
+                        break;
+                    case 'L':
+                        Lline(linea.Trim());
+                        break;
+                    case 'V':
+                        break;
+                    case 'H':
+                        break;
+                    case 'F':
+                        break;
+                    case 'S':
+                        break;
+                    case 'X':
                         break;
                     default:
+                        MessageBox.Show("Linea no válida: " + linea);
                         break;
                 }
 
             }
         }
 
+        #region Lineas
         private void Iline(string line)
         {
             string[] list = line.Split(' ');
@@ -43,7 +61,7 @@ namespace WindowsFormsAppTestConekta
             {
                 if (list.Length == 3)
                 {
-                    if (list[0] != "I")
+                    if (list[0].Length != 1)
                         throw new Exception();
                     if (!int.TryParse(list[1], out M))
                         throw new Exception();
@@ -58,17 +76,74 @@ namespace WindowsFormsAppTestConekta
             }
             catch (Exception)
             {
-
                 MessageBox.Show("Linea no válida: " + line);
             }
-
         }
 
+        private void Cline(string line)
+        {
+            if (line.Length == 1)
+            {
+                clearImage();
+            }
+            else
+            {
+                MessageBox.Show("Linea no válida: " + line);
+            }
+        }
+
+        private void Lline(string line)
+        {
+            int x = 0, y = 0;
+            string[] list = line.Split(' ');
+            try
+            {
+                if (list.Length == 4)
+                {
+                    if (list[0].Length != 1)
+                        throw new Exception();
+                    if (!int.TryParse(list[1], out x))
+                        throw new Exception();
+                    if (!int.TryParse(list[2], out y))
+                        throw new Exception();
+                    if (list[3].Length != 1)
+                        throw new Exception();
+                    paintPixel(x,y,char.Parse(list[3]));
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Linea no válida: " + line);
+            }
+        }
+        #endregion Lineas
+
+        #region Aux
         private void createImage(int M, int N)
         {
-            MessageBox.Show("creaimagen: " + M + " " + N);
-            image = new int[M, N];
+            image = new char[M, N];
+            clearImage();
         }
-
+        private void clearImage()
+        {
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    image[i, j] = 'O';
+                }
+            }
+        }
+        private void paintPixel(int x, int y, char color)
+        {
+            x -= 1;
+            y -= 1;
+            image[x, y] = color;
+        }
+        #endregion
     }
 }
