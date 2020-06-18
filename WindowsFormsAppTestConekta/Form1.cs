bea8b1f4b99219c1754ddfe03103abe9b9cc445c
omarus85 <52642246+omarus85@users.noreply.github.com>
2020-06-17 23:35:29 -0500

    Finishing touches and comments

--- WindowsFormsAppTestConekta/Form1.cs
+++ WindowsFormsAppTestConekta/Form1.cs
@@ -23,49 +23,69 @@ namespace WindowsFormsAppTestConekta
         private void buttonAccept_Click(object sender, EventArgs e)
         {
             textBoxOutput.Text = string.Empty;
-            foreach (string linea in textBoxInput.Lines)
+            if (textBoxInput.Lines.Length > 0)
             {
-                try
+                evaluaLineas(textBoxInput.Lines);
+            }
+        }
+        /// <summary>
+        /// Evalua linea por linea la información ingresada.
+        /// </summary>
+        /// <param name="lineas">Arreglo con la informacíon a evaluar</param>
+        private void evaluaLineas(string[] lineas)
+        {
+            foreach (string line in lineas)
+            {
+                string linea = line.Trim();
+                if (linea.Length > 0)
                 {
-                    switch (linea[0])
+                    try
                     {
-                        case 'I':
-                            Iline(linea.Trim());
-                            break;
-                        case 'C':
-                            Cline(linea.Trim());
-                            break;
-                        case 'L':
-                            Lline(linea.Trim());
-                            break;
-                        case 'V':
-                            Vline(linea.Trim());
-                            break;
-                        case 'H':
-                            Hline(linea.Trim());
-                            break;
-                        case 'F':
-                            Fline(linea.Trim());
-                            break;
-                        case 'S':
-                            Sline(linea.Trim());
-                            break;
-                        case 'X':
-                            Xline(linea.Trim());
-                            break;
-                        default:
-                            MessageBox.Show("Linea no válida: " + linea);
-                            break;
+                        switch (linea[0])
+                        {
+                            case 'I':
+                                Iline(linea);
+                                break;
+                            case 'C':
+                                Cline(linea);
+                                break;
+                            case 'L':
+                                Lline(linea);
+                                break;
+                            case 'V':
+                                Vline(linea);
+                                break;
+                            case 'H':
+                                Hline(linea);
+                                break;
+                            case 'F':
+                                Fline(linea);
+                                break;
+                            case 'S':
+                                Sline(linea);
+                                break;
+                            case 'X':
+                                Xline(linea);
+                                break;
+                            default:
+                                throw new Exception();
+                                break;
+                        }
                     }
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Linea no válida: " + linea);
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Linea no válida: " + linea);
+                        break;
+                    }
                 }
             }
         }
 
         #region Lineas
+        /// <summary>
+        /// Valida las lineas que empiezan con la letra I
+        /// </summary>
+        /// <param name="line">linea a evaluar</param>
         private void Iline(string line)
         {
             string[] list = line.Split(' ');
@@ -91,6 +111,10 @@ namespace WindowsFormsAppTestConekta
                 throw new Exception();
             }
         }
+        /// <summary>
+        /// Valida las lineas que empiezan con la letra C
+        /// </summary>
+        /// <param name="line">linea a evaluar</param>
         private void Cline(string line)
         {
             if (line.Length == 1)
@@ -102,6 +126,10 @@ namespace WindowsFormsAppTestConekta
                 throw new Exception();
             }
         }
+        /// <summary>
+        /// Valida las lineas que empiezan con la letra L
+        /// </summary>
+        /// <param name="line">linea a evaluar</param>
         private void Lline(string line)
         {
             int x = 0, y = 0;
@@ -132,6 +160,10 @@ namespace WindowsFormsAppTestConekta
                 throw new Exception();
             }
         }
+        /// <summary>
+        /// Valida las lineas que empiezan con la letra S
+        /// </summary>
+        /// <param name="line">linea a evaluar</param>
         private void Sline(string line)
         {
             StringBuilder builder = new StringBuilder();
@@ -145,6 +177,10 @@ namespace WindowsFormsAppTestConekta
                 textBoxOutput.AppendText(builder.ToString() + "\r\n");
             }
         }
+        /// <summary>
+        /// Valida las lineas que empiezan con la letra V
+        /// </summary>
+        /// <param name="line">linea a evaluar</param>
         private void Vline(string line)
         {
             int x = 0, y1 = 0, y2 = 0;
@@ -177,6 +213,10 @@ namespace WindowsFormsAppTestConekta
                 throw new Exception();
             }
         }
+        /// <summary>
+        /// Valida las lineas que empiezan con la letra H
+        /// </summary>
+        /// <param name="line">linea a evaluar</param>
         private void Hline(string line)
         {
             int y = 0, x1 = 0, x2 = 0;
@@ -209,6 +249,11 @@ namespace WindowsFormsAppTestConekta
                 throw new Exception();
             }
         }
+        /// <summary>
+        /// Valida las lineas que empiezan con la letra X
+        /// Como es la instrucción que termina la sesión, deshabilita el boton aceptar para no modificar el resultado.
+        /// </summary>
+        /// <param name="line">linea a evaluar</param>
         private void Xline(string line)
         {
             if (line.Length == 1)
@@ -220,13 +265,16 @@ namespace WindowsFormsAppTestConekta
                 throw new Exception();
             }
         }
+        /// <summary>
+        /// valida las lineas que empiezan con la letra F
+        /// </summary>
+        /// <param name="line">linea a evaluar</param>
         private void Fline(string line)
         {
             int x = 0, y = 0;
             string[] list = line.Split(' ');
             try
             {
-               
                 if (list.Length == 4)
                 {
                     if (list[0].Length != 1)
@@ -254,11 +302,19 @@ namespace WindowsFormsAppTestConekta
         #endregion Lineas
 
         #region Aux
+        /// <summary>
+        /// Crea la imagen y la inicializa
+        /// </summary>
+        /// <param name="M">Ancho de la imagen</param>
+        /// <param name="N">Altura de la imagen</param>
         private void createImage(int M, int N)
         {
             image = new char[M, N];
             clearImage();
         }
+        /// <summary>
+        /// Elimina todo el contenido de la imagen.
+        /// </summary>
         private void clearImage()
         {
             for (int i = 0; i < M; i++)
@@ -268,13 +324,27 @@ namespace WindowsFormsAppTestConekta
                     image[i, j] = 'O';
                 }
             }
+            textBoxOutput.Text = string.Empty;
         }
+        /// <summary>
+        /// Cambia el color del pixel indicado
+        /// </summary>
+        /// <param name="x">Coordenada x del pixel a cambiar</param>
+        /// <param name="y">Coordenada y del pixel a cambiar</param>
+        /// <param name="color"></param>
         private void paintPixel(int x, int y, char color)
         {
             x -= 1;
             y -= 1;
             image[x, y] = color;
         }
+        /// <summary>
+        /// Pinta una linea vertical
+        /// </summary>
+        /// <param name="x">Indice de la columna a pintar</param>
+        /// <param name="y1">Renglon inicial de la posición a pintar</param>
+        /// <param name="y2">Renglon final de la posición a pintar</param>
+        /// <param name="color"></param>
         private void paintVLine(int x, int y1, int y2, char color)
         {
             int min = y1 < y2 ? y1 : y2;
@@ -287,6 +357,13 @@ namespace WindowsFormsAppTestConekta
                 image[x, y] = color;
             }
         }
+        /// <summary>
+        /// Pinta una linea horizontal
+        /// </summary>
+        /// <param name="x1">Columna inicial de la posición a pintar</param>
+        /// <param name="x2">Columna final de la posición a pintar</param>
+        /// <param name="y">Indice del renglon a pintar</param>
+        /// <param name="color"></param>
         private void paintHLine(int x1, int x2, int y, char color)
         {
             int min = x1 < x2 ? x1 : x2;
@@ -299,7 +376,12 @@ namespace WindowsFormsAppTestConekta
                 image[x, y] = color;
             }
         }
-
+        /// <summary>
+        /// Pinta el pixel seleccionado del color indicado y evalua si también se deben de pintar los pixeles contiguos
+        /// </summary>
+        /// <param name="x">Coordenada x del pixel a cambiar</param>
+        /// <param name="y">Coordenada y del pixel a cambiar</param>
+        /// <param name="color">El color a cambiar</param>
         private void fillRegion(int x, int y, char color)
         {
             char originalColor = image[x - 1, y - 1];
@@ -333,7 +415,12 @@ namespace WindowsFormsAppTestConekta
                 }
             }
         }
-
+        /// <summary>
+        /// Obtiene el color de la posición seleccionada
+        /// </summary>
+        /// <param name="x">Coordenada x del pixel a cambiar</param>
+        /// <param name="y">Coordenada y del pixel a cambiar</param>
+        /// <returns>El color de la posición seleccionada</returns>
         private char getColor(int x, int y)
         {
             return image[x - 1, y - 1];
